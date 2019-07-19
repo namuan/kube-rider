2294d180320f24760225dd24f56b333aa76923f6
namuan <575441+namuan@users.noreply.github.com>
2019-07-19 07:59:32 +0100

    [Feature] Added configuration to input kubectl path

--- kuberider/ui/configuration_dialog.py
+++ kuberider/ui/configuration_dialog.py
@@ -1,17 +1,15 @@
 from PyQt5.QtWidgets import (QDialog)
 
-from ..presenters.configuration_presenter import ConfigurationPresenter
 from ..generated.configuration_dialog import Ui_Configuration
+from ..presenters.configuration_presenter import ConfigurationPresenter
 
 
 class ConfigurationDialog(QDialog, Ui_Configuration):
 
     def __init__(self, parent=None):
         super(ConfigurationDialog, self).__init__(parent)
-        self.presenter = ConfigurationPresenter(self, parent)
-
-    def initialize(self):
         self.setupUi(self)
+        self.presenter = ConfigurationPresenter(self, parent)
 
     def show_dialog(self):
         self.presenter.load_configuration_dialog()

ef4b6fce30a67437bc94aa8a5daf11fd589dd889
namuan <575441+namuan@users.noreply.github.com>
2019-07-28 16:36:30 +0100

    [Improv] Code cleanup

--- kuberider/widgets/pod_item_widget.py
+++ kuberider/widgets/pod_item_widget.py
@@ -1,18 +1,19 @@
 from PyQt5 import QtWidgets
 
-from kuberider.entities.model import KubePodItem
+from ..entities.model import KubePodItem
 from ..generated.pod_item_widget import Ui_PodItemWidget
 
 
 class PodItemWidget(QtWidgets.QWidget, Ui_PodItemWidget):
+    pod_info: KubePodItem
 
     def __init__(self, pod_info: KubePodItem, parent=None):
         super(PodItemWidget, self).__init__(parent)
         self.setupUi(self)
-        self.pod_info = pod_info
         self.set_data(pod_info)
 
     def set_data(self, pod_info: KubePodItem):
+        self.pod_info = pod_info
         self.lbl_pod_name.setText(pod_info.name)
         self.lbl_pod_count.setText(pod_info.count)
         self.lbl_pod_status.setText(pod_info.pod_status)

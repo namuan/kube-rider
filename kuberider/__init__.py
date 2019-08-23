d92d0b6388af02aaf425930194d518764ed912e1
namuan <575441+namuan@users.noreply.github.com>
2019-08-23 18:28:06 +0100

    Release 0.0.2
    - Bundling icon for MacOS release

--- kuberider/__init__.py
+++ kuberider/__init__.py
@@ -1,4 +1,7 @@
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __appname__ = 'kuberider'
 __description__ = 'Kubernetes Desktop Application'
 __desktopid__ = 'com.deskriders.KubeRider'
+
+import sqlalchemy.dialects.sqlite
+import sqlalchemy.sql.default_comparator
